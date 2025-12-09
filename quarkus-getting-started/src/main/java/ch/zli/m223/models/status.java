cf62e1bf4cc625c5ec8b574388b0408d48f07940
bbw-tes <sabina.teleskumar@lernende.bbw.ch>
2025-12-09 09:40:56 +0000

    DB anbindung zur App

--- quarkus-getting-started/src/main/java/ch/zli/m223/models/status.java
+++ quarkus-getting-started/src/main/java/ch/zli/m223/models/status.java
@@ -1,2 +1,17 @@
 package ch.zli.m223.models;
 
+import io.quarkus.arc.impl.Identified;
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "status")
+
+public class Status() {
+    @Id 
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+}
